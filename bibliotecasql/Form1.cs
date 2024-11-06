5f5f184c00daba2757b464e5f908f4d30e1e8a9d
Rodrigo Pinto <164094140+rodrigofpinto@users.noreply.github.com>
2024-11-06 19:33:28 +0000

    up

--- bibliotecasql/Form1.cs
+++ bibliotecasql/Form1.cs
@@ -12,11 +12,45 @@ namespace bibliotecasql
 {
     public partial class Form1 : Form
     {
+        // Fields to enable dragging
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Attach the drag events to the form
+            this.MouseDown += new MouseEventHandler(Form_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form_MouseMove);
+            this.MouseUp += new MouseEventHandler(Form_MouseUp);
         }
 
+        // Dragging methods
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                // Update the form's position while dragging
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(diff));
+            }
+        }
+
+        private void Form_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        // Other event methods
         private void label2_Click(object sender, EventArgs e)
         {
 
@@ -25,6 +59,21 @@ namespace bibliotecasql
         private void tabPage1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
